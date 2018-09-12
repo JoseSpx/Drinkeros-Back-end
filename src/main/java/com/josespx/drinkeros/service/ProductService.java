88d1c78e545751b3284511f802c835f32ffc1e5b
Jose Suarez <jose95sp@outlook.com>
2018-09-11 23:52:04 -0500

    added productService

---
+++ src/main/java/com/josespx/drinkeros/service/ProductService.java
@@ -0,0 +1,21 @@
+package com.josespx.drinkeros.service;
+
+import com.josespx.drinkeros.dao.Dao;
+import com.josespx.drinkeros.model.Product;
+
+import java.util.List;
+
+public interface ProductService extends Dao<Product, Long> {
+
+    @Override
+    void save(Product product);
+
+    @Override
+    Product findById(Long id);
+
+    @Override
+    List<Product> findAll();
+
+    @Override
+    void delete(Long id);
+}
---
+++ src/main/java/com/josespx/drinkeros/service/ProductServiceImpl.java
@@ -0,0 +1,41 @@
+package com.josespx.drinkeros.service;
+
+import com.josespx.drinkeros.dao.ProductDao;
+import com.josespx.drinkeros.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+@Service(value = "productService")
+public class ProductServiceImpl implements ProductService {
+
+    private ProductDao productDao;
+
+    @Autowired
+    public ProductServiceImpl(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    @Override
+    public void save(Product product) {
+        this.productDao.save(product);
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return this.productDao.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return this.productDao.findAll();
+    }
+
+    @Override
+    public void delete(Long id) {
+        this.productDao.deleteById(id);
+    }
+}
