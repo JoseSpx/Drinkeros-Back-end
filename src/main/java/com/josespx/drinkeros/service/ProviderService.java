f3c62f909708754c8e26dfef9d5bd884e324a31c
Jose Suarez <jose95sp@outlook.com>
2018-09-11 23:58:29 -0500

    added providerService

---
+++ src/main/java/com/josespx/drinkeros/service/ProviderService.java
@@ -0,0 +1,21 @@
+package com.josespx.drinkeros.service;
+
+import com.josespx.drinkeros.dao.Dao;
+import com.josespx.drinkeros.model.Provider;
+
+import java.util.List;
+
+public interface ProviderService extends Dao<Provider, Long> {
+
+    @Override
+    void save(Provider provider);
+
+    @Override
+    Provider findById(Long id);
+
+    @Override
+    List<Provider> findAll();
+
+    @Override
+    void delete(Long id);
+}
---
+++ src/main/java/com/josespx/drinkeros/service/ProviderServiceImpl.java
@@ -0,0 +1,41 @@
+package com.josespx.drinkeros.service;
+
+import com.josespx.drinkeros.dao.ProviderDao;
+import com.josespx.drinkeros.model.Provider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+@Service(value = "providerService")
+public class ProviderServiceImpl implements ProviderService {
+
+    private ProviderDao providerDao;
+
+    @Autowired
+    public ProviderServiceImpl(ProviderDao providerDao) {
+        this.providerDao = providerDao;
+    }
+
+    @Override
+    public void save(Provider provider) {
+        this.providerDao.save(provider);
+    }
+
+    @Override
+    public Provider findById(Long id) {
+        return this.providerDao.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Provider> findAll() {
+        return this.providerDao.findAll();
+    }
+
+    @Override
+    public void delete(Long id) {
+        this.providerDao.deleteById(id);
+    }
+}
