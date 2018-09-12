c18f219df3e189896bc513353d082645c7816be7
Jose Suarez <jose95sp@outlook.com>
2018-09-11 19:57:43 -0500

    added typeDocument entity

--- src/main/java/com/josespx/drinkeros/model/Client.java
+++ src/main/java/com/josespx/drinkeros/model/Client.java
@@ -35,4 +35,88 @@ public class Client {
     @Column(name = "updated_at")
     private String updatedAt;
 
+    @OneToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    private TypeDocument typeDocument;
+
+    public Client() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public TypeDocument getTypeDocument() {
+        return typeDocument;
+    }
+
+    public void setTypeDocument(TypeDocument typeDocument) {
+        this.typeDocument = typeDocument;
+    }
 }
---
+++ src/main/java/com/josespx/drinkeros/model/TypeDocument.java
@@ -0,0 +1,45 @@
+package com.josespx.drinkeros.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "type_document")
+public class TypeDocument {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(mappedBy = "typeDocument")
+    private Client client;
+
+    public TypeDocument() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
